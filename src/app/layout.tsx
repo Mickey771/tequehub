a4fbfddd905287d35d45c320850487b73f1e045d
igee-17 <olaleye@unifiedbeez.com>
2026-01-14 00:24:40 +0100

    initiated seo indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Teque</title>
+        <meta
+          name="description"
+          content="We offer the best creative solution through tech"
+        />
+
+        {/* Open Graph Meta Tags (Facebook, LinkedIn) */}
+        <meta property="og:title" content="Teque" />
+        <meta
+          property="og:description"
+          content="We offer the best creative solution through tech"
+        />
+        <meta property="og:image" content="https://www.teque.tech/logo.jpg" />
+        <meta property="og:url" content="https://www.teque.tech" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Teque" />
+
+        {/* Optional: Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
         <MainLayout>{children}</MainLayout>
       </body>
